df9c0e6fe256bd54ba31efdc1668bac639fbc8aa
이정은 <jeongeun1223@gmail.com>
2024-07-23 18:30:03 +0900

    Q34 : a와 b 출력하기

---
+++ Programmers/src/Lv0/Q34.java
@@ -0,0 +1,27 @@
+package Lv0;
+
+import java.util.*;
+
+/*
+	< 코딩 기초 트레이닝 : a와 b 출력하기 >
+	
+	✔️결과 : +1
+	
+	정수 a와 b가 주어집니다.
+	각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
+	
+	제한사항)
+	-100,000 ≤ a, b ≤ 100,000
+*/
+
+public class Q34 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        //sc.close();
+    }
+}
